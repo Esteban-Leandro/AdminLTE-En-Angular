a8cb2cbaa66ffef707427ae89ff769f03ce60be7
Esteban-Leandro <esteban.vilchesf@gmail.com>
2020-04-28 21:34:32 -0400

    Se adpato el tamplate AdminLTE v3.0.4 con angular esta funcionando correctamente, ahora comenzare a adaptarlo a mis necesidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { MainHeaderComponent } from './pages/main-header/main-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    MainHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/assets/plugins/filterizr/FilterItems.d.ts
@@ -0,0 +1,22 @@
+import ActiveFilter from './ActiveFilter';
+import FilterItem from './FilterItem';
+import FilterizrOptions from './FilterizrOptions/FilterizrOptions';
+export default class FilterItems {
+    private filterItems;
+    private options;
+    constructor(filterItems: FilterItem[], options: FilterizrOptions);
+    readonly length: number;
+    get(): FilterItem[];
+    getItem(index: number): FilterItem;
+    set(filterItems: FilterItem[]): void;
+    destroy(): void;
+    updateTransitionStyle(): void;
+    updateDimensions(): void;
+    push(filterItem: FilterItem): number;
+    getFiltered(filter: ActiveFilter): FilterItem[];
+    getFilteredOut(filter: ActiveFilter): FilterItem[];
+    getSorted(sortAttr?: string, sortOrder?: 'asc' | 'desc'): FilterItem[];
+    getSearched(searchTerm: string): FilterItem[];
+    getShuffled(): FilterItem[];
+    private shouldBeFiltered;
+}
